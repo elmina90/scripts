1f914cf034a17260628b9cad9c8e642075935358
Joe B <budigunawanjohanes@gmail.com>
2016-06-23 23:54:41 +0700

    Update port and mongodb version

--- packages/mongodb.sh
+++ packages/mongodb.sh
@@ -8,8 +8,8 @@
 #
 # Include in your builds via
 # \curl -sSL https://raw.githubusercontent.com/codeship/scripts/master/packages/mongodb.sh | bash -s
-MONGODB_VERSION=${MONGODB_VERSION:="3.0.4"}
-MONGODB_PORT=${MONGODB_PORT:="27018"}
+MONGODB_VERSION=${MONGODB_VERSION:="2.4.9"}
+MONGODB_PORT=${MONGODB_PORT:="27019"}
 MONGODB_DIR=${MONGODB_DIR:="$HOME/mongodb"}
 MONGODB_WAIT_TIME=${MONGODB_WAIT_TIME:="10"}
 
@@ -17,7 +17,7 @@ set -e
 CACHED_DOWNLOAD="${HOME}/cache/mongodb-linux-x86_64-ubuntu1404-${MONGODB_VERSION}.tgz"
 
 mkdir -p "${MONGODB_DIR}"
-wget --continue --output-document "${CACHED_DOWNLOAD}" "https://fastdl.mongodb.org/linux/mongodb-linux-x86_64-ubuntu1404-${MONGODB_VERSION}.tgz"
+wget --continue --output-document "${CACHED_DOWNLOAD}" "https://fastdl.mongodb.org/linux/mongodb-linux-x86_64-${MONGODB_VERSION}.tgz"
 tar -xaf "${CACHED_DOWNLOAD}" --strip-components=1 --directory "${MONGODB_DIR}"
 
 # Make sure to use the exact parameters you want for MongoDB and give it enough sleep time to properly start up
