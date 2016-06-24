1543cd79550387cd5935f01be51155653f461d5f
Joe B <budigunawanjohanes@gmail.com>
2016-06-24 22:28:25 +0700

    update port

--- packages/mongodb.sh
+++ packages/mongodb.sh
@@ -9,7 +9,7 @@
 # Include in your builds via
 # \curl -sSL https://raw.githubusercontent.com/codeship/scripts/master/packages/mongodb.sh | bash -s
 MONGODB_VERSION=${MONGODB_VERSION:="3.2.4"}
-MONGODB_PORT=${MONGODB_PORT:="27021"}
+MONGODB_PORT=${MONGODB_PORT:="27017"}
 MONGODB_DIR=${MONGODB_DIR:="$HOME/mongodb"}
 MONGODB_WAIT_TIME=${MONGODB_WAIT_TIME:="10"}
 
